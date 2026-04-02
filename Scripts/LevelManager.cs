9414283a36e9a3b8651228d93ad0852275ab7aa8
SumSmoker <sumsmoker@gmail.com>
2026-04-02 17:13:55 -0400

    Update LevelManager.cs

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -7,7 +7,11 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField]
     private PlayerController player;
+    [SerializeField]
     private Damage enemyScript;
+    [SerializeField]
+    private EnemySpawner enemySpawner;
+
     public int lootCount;
     public TextMeshProUGUI lootText;
     public int killCount;
@@ -17,13 +21,40 @@ public class LevelManager : MonoBehaviour
     {
         player = FindObjectOfType<PlayerController>();
         enemyScript = FindObjectOfType<Damage>();
+        enemySpawner = FindObjectOfType<EnemySpawner>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //set up UI text and keep it updated
         lootText.text = "" + lootCount;
         killText.text = "" + killCount;
+
+        //reset non-player elements when killed
+        if(player.currentHealth <= 0)
+        {
+            resetEnemies();
+        }
+    }
+
+    public void resetEnemies()
+    {
+        GameObject[] spawners = GameObject.FindGameObjectsWithTag("Spawner"); //find every enemy spawner and put in array
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); //find every enemy and put in array
+        
+        //goes through the array of enemies and destroys them; make sure the enemy prefab has the "Enemy" tag
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            Destroy(enemies[i]);
+        }
+        //goes through the array of spawners and resets them; the spawner prefab should already have the "Spawner" tag
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            spawners[i].GetComponent<EnemySpawner>().StopAllCoroutines(); //stop enemy instantiating coroutine
+            spawners[i].GetComponent<CapsuleCollider2D>().enabled = true; //set zone trigger to true so the player can collide with it again and start the spawning coroutine
+        }
     }
 
     public void AddLoot(int lootToAdd)
