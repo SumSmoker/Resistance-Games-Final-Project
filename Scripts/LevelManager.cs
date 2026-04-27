dd9f7fa5d5f47dd9e2596e36410882e2fb6a825f
SumSmoker <sumsmoker@gmail.com>
2026-04-27 05:07:46 -0400

    Update LevelManager.cs
    Now includes Gate resetting, and fixes a leftover DeathTimer time bug by stopping PlayerController coroutines on start

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -8,7 +8,7 @@ public class LevelManager : MonoBehaviour
     [SerializeField]
     private PlayerController player;
     [SerializeField]
-    private Damage enemyScript;
+    private EnemyHealth enemyScript;
     [SerializeField]
     private EnemySpawner enemySpawner;
 
@@ -20,8 +20,9 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
-        enemyScript = FindObjectOfType<Damage>();
+        enemyScript = FindObjectOfType<EnemyHealth>();
         enemySpawner = FindObjectOfType<EnemySpawner>();
+        player.StopAllCoroutines();
     }
 
     // Update is called once per frame
@@ -29,11 +30,11 @@ public class LevelManager : MonoBehaviour
     {
 
         //set up UI text and keep it updated
-        lootText.text = "" + lootCount;
+        lootText.text = "" + player.coins;
         killText.text = "" + killCount;
 
         //reset non-player elements when killed
-        if(player.currentHealth <= 0)
+        if (player.currentHealth <= 0)
         {
             resetEnemies();
         }
@@ -43,26 +44,34 @@ public class LevelManager : MonoBehaviour
     {
         GameObject[] spawners = GameObject.FindGameObjectsWithTag("Spawner"); //find every enemy spawner and put in array
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); //find every enemy and put in array
-        
+        GameObject[] loot = GameObject.FindGameObjectsWithTag("Loot"); //find every enemy and put in array
+        GameObject[] gates = GameObject.FindGameObjectsWithTag("Gate"); //find every enemy and put in array
+
+
         //goes through the array of enemies and destroys them; make sure the enemy prefab has the "Enemy" tag
         for (int i = 0; i < enemies.Length; i++)
         {
             Destroy(enemies[i]);
-        }
+        //}
         //goes through the array of spawners and resets them; the spawner prefab should already have the "Spawner" tag
-        for (int i = 0; i < enemies.Length; i++)
-        {
+        //for (int i = 0; i < enemies.Length; i++)
+        //{
             spawners[i].GetComponent<EnemySpawner>().StopAllCoroutines(); //stop enemy instantiating coroutine
+            spawners[i].GetComponent<EnemySpawner>().hasEntered = false;
             spawners[i].GetComponent<CapsuleCollider2D>().enabled = true; //set zone trigger to true so the player can collide with it again and start the spawning coroutine
+        }
+        for (int i = 0; i < loot.Length; i++)
+        {
+            Destroy(loot[i]);
+        }
+        for (int i = 0; i < gates.Length; i++)
+        {
+            gates[i].GetComponent<CapsuleCollider2D>().enabled = true;
+            gates[i].GetComponent<BoxCollider2D>().enabled = true;
+            gates[i].GetComponent<SpriteRenderer>().enabled = true;
         }
     }
 
-    public void AddLoot(int lootToAdd)
-    {
-        player.addToLoot(lootToAdd);
-        lootCount = lootCount + lootToAdd;
-        lootText.text = "" + lootCount;
-    }
     public void Addkills(int killsToAdd)
     {
         killCount = killCount + killsToAdd;
